7f74730dcffdefc2c7e413edd7e7cdeb4204f828
azrapallan <pallanazra@gmail.com>
2026-01-02 15:24:35 +0530

    Update test_product.py

--- test_product.py
+++ test_product.py
@@ -1,25 +1,31 @@
 from product import avg, status
 
+
 def test_average_rating():
     ratings = [4, 5, 3, 4]
     assert avg(ratings) == 4.0
+
 
 def test_average_empty():
     ratings = []
     assert avg(ratings) == 0
 
+
 def test_status_excellent():
-    avg = 4.7
-    assert product_status(avg) == "Excellent"
+    avg_rating = 4.7
+    assert status(avg_rating) == "Excellent"
+
 
 def test_status_good():
-    avg = 3.8
-    assert status(avg) == "Good"
+    avg_rating = 3.8
+    assert status(avg_rating) == "Good"
+
 
 def test_status_average():
-    avg = 2.9
-    assert status(avg) == "Average"
+    avg_rating = 2.9
+    assert status(avg_rating) == "Average"
+
 
 def test_status_poor():
-    avg = 2.0
-    assert status(avg) == "Poor"
+    avg_rating = 2.0
+    assert status(avg_rating) == "Poor"
